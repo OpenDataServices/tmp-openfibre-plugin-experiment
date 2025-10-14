4dd2f4fbf83f490506c52e610202cc66aa6b86c4
James B <james.baster@opendataservices.coop>
2025-10-14 11:26:31 +0100

    Start
    (Thanks https://courses.spatialthoughts.com/qgis-plugin-workshop.html !)

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtGui import QIcon
+
+plugin_dir = os.path.dirname(__file__)
+
+class BasemapLoaderPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+
+    def initGui(self):
+        # Create an action (i.e. a button) with Logo
+        icon = os.path.join(os.path.join(plugin_dir, 'logo.png'))
+        self.action = QAction(QIcon(icon), 'Test', self.iface.mainWindow())
+        # Add the action to the toolbar
+        self.iface.addToolBarIcon(self.action)
+        # Connect the run() method to the action
+        self.action.triggered.connect(self.run)
+      
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
+        
+    def run(self):
+        self.iface.messageBar().pushMessage('Hello from Plugin')
+        
